643e66a152ab669d36c0ea60a77c309688319e8c
Sam Davis <sam@sjd.co>
2022-04-09 16:21:18 +1000

    update link resolver

--- prismicio.js
+++ prismicio.js
@@ -10,6 +10,8 @@ export function linkResolver(doc) {
     switch (doc.type) {
         case "home-page":
             return "/";
+        case "about-page":
+            return "/about";
         case "project":
             return `/project/${doc.uid}`;
         default:
